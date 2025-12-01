fedfb96e0b871464f762d537296a2c62f8c3430b
Meet Mendapara <122281320+Meetmendapara09@users.noreply.github.com>
2025-12-01 14:53:09 +0530

    Refactor input handling and counting logic

--- 1.cpp
+++ 1.cpp
@@ -1,30 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    string line;
-    int position = 50;        
-    int countZero = 0;        
-
-    while (getline(cin, line)) {
-        if (line.empty()) continue;
-
-        char dir = line[0];               
-        // 'L' or 'R'
-        int dist = stoi(line.substr(1));
-
-        if (dir == 'L') {
-            position = (position - dist) % 100;
-            if (position < 0) position += 100;
-        } else if (dir == 'R') {
-            position = (position + dist) % 100;
-        }
-
-        if (position == 0) {
-            countZero++;
-        }
+    int p = 50, z = 0, d;
+    string s;
+    while (cin >> s) {
+        d = stoi(s.substr(1));
+        if (s[0] == 'L') 
+            p = (p - d % 100 + 100) % 100;
+        else 
+            p = (p + d) % 100;
+        if (!p) 
+            z++;
     }
-
-    cout << countZero << "\n";
-    return 0;
+    cout << z;
 }
